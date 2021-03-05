ed87576243e12aa6e952958690318ff1d06f3b0d
rtub5 <77343357+rtub5@users.noreply.github.com>
2021-03-05 19:23:52 +0100

    Update COIN-FLIPPER.py

--- COIN-FLIPPER.py
+++ COIN-FLIPPER.py
@@ -2,23 +2,25 @@ import random
 import time
 
 print("  ____ ___ ___ _   _       _____ _     ___ ____  ____  _____ ____  ")
-print(" / ___/ _ \_ _| \ | |     |  ___| |   |_ _|  _ \|  _ \| ____|  _ \
-")
+print(" / ___/ _ \_ _| \ | |     |  ___| |   |_ _|  _ \|  _ \| ____|  _ \")
 print("| |  | | | | ||  \| |_____| |_  | |    | || |_) | |_) |  _| | |_) |")
 print("| |__| |_| | || |\  |_____|  _| | |___ | ||  __/|  __/| |___|  _ < ")
-print(" \____\___/___|_| \_|     |_|   |_____|___|_|   |_|   |_____|_| \_\
-")
+print(" \____\___/___|_| \_|     |_|   |_____|___|_|   |_|   |_____|_| \_\")
 print("                   Made by Roger Bajona") 
-    
+          
 
 
+number_of_flip_coins = 10 #insert how many coins you wanna flip
 
-number_of_flip_coins = 10
-
-face = 0
+face = 0 
 cross = 0
 
 total_throws = 0
+
+percent_face = face / total_throws * 100 
+percent_cross = cross / total_throws * 100 
+      
+      
 
 while number_of_flip_coins != total_throws:
     if random.randint(1, 2) == 1:
@@ -28,20 +30,26 @@ while number_of_flip_coins != total_throws:
         cross += 1 
         total_throws += 1 
 
-print(face)
-print(cross)
-print(total_throws)
+print("Faces: " + face)
+print("Crosses: "cross)
+print("Total throws" + total_throws)
+print("Percent of cross" + percent_cross)
+print("Percent of faces" + percent_face)
+
 
 save_question = input("Do you want to export the data?")
+print("If you want to save, write #yes# otherwise write #no#")
 
 if save_question == "yes":
     print("saving")
     file_name = input("How do you want do name your file")
     f = (file_name, "x")
     f1 = (file_name, "w") 
-    f1.write("Number of faces:" face)
-    f1.write("Number of crosses" cross)
-    f1.write("Total flipps" total_throws)
+    f1.write("Number of faces: " face)
+    f1.write("Number of crosses: " cross)
+    f1.write("Total flipps :" total_throws)
+    f1.write("Percent of faces: " percent_face) 
+    f1.write("Percent of crosses" percent_cross) 
     f1.close
     f.close
 elif save_question == "no":
